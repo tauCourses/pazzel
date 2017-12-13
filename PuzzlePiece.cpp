e8ee7f2b137127c8fa46b3efc37ed1d80ba8bb0b
Ido Kessler <t-idkess@microsoft.com>
2017-12-13 23:06:42 +0200

    switch order - apperantly matters

--- PuzzlePiece.cpp
+++ PuzzlePiece.cpp
@@ -1,10 +1,10 @@
 #include "PuzzlePiece.h"
 
 PuzzlePiece::PuzzlePiece(int index, int left, int up, int right, int down) :
-        index(index), left(left), up(up), down(down), right(right) {}
+        index(index), left(left), up(up), right(right), down(down) {}
 
 Piece_t PuzzlePiece::representor() {
-    return (Piece_t) ( (left << 6) | (up << 4) | (right << 2) | down );
+    return (Piece_t) ((left << 6) | (up << 4) | (right << 2) | down);
 }
 
 PuzzlePiece::PuzzlePiece(const PuzzlePiece *copy) :
@@ -18,16 +18,16 @@ bool PuzzlePiece::isValidPiece(Piece_t piece) {
 }
 
 bool PuzzlePiece::isPieceRespectConstrain(Piece_t p, Piece_t c) {
-    Piece_t p1 = static_cast<Piece_t>(p >> 6 & 0x3);
-    Piece_t p2 = static_cast<Piece_t>(p >> 4 & 0x3);
-    Piece_t p3 = static_cast<Piece_t>(p >> 2 & 0x3);
-    Piece_t p4 = static_cast<Piece_t>(p >> 0 & 0x3);
-    Piece_t c1 = static_cast<Piece_t>(c >> 6 & 0x3);
-    Piece_t c2 = static_cast<Piece_t>(c >> 4 & 0x3);
-    Piece_t c3 = static_cast<Piece_t>(c >> 2 & 0x3);
-    Piece_t c4 = static_cast<Piece_t>(c >> 0 & 0x3);
+    auto p1 = static_cast<Piece_t>(p >> 6 & 0x3);
+    auto p2 = static_cast<Piece_t>(p >> 4 & 0x3);
+    auto p3 = static_cast<Piece_t>(p >> 2 & 0x3);
+    auto p4 = static_cast<Piece_t>(p >> 0 & 0x3);
+    auto c1 = static_cast<Piece_t>(c >> 6 & 0x3);
+    auto c2 = static_cast<Piece_t>(c >> 4 & 0x3);
+    auto c3 = static_cast<Piece_t>(c >> 2 & 0x3);
+    auto c4 = static_cast<Piece_t>(c >> 0 & 0x3);
     return (c1 == 0x3 || c1 == p1) &&
-           (c2==0x3 || c2 == p2) &&
-           (c3==0x3 || c3 == p3) &&
-           (c4==0x3 || c4 == p4);
+           (c2 == 0x3 || c2 == p2) &&
+           (c3 == 0x3 || c3 == p3) &&
+           (c4 == 0x3 || c4 == p4);
 }
